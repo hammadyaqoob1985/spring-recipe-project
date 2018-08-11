a03d1358f926705f8c90321d90200d390896a1e5
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-08-11 16:12:21 +0100

    changed controller to autowire via constructor added test for index contoller via mocks

--- src/main/java/guru/springframework/recipe/project/recipeproject/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipe/project/recipeproject/controllers/IndexController.java
@@ -3,7 +3,6 @@ package guru.springframework.recipe.project.recipeproject.controllers;
 import guru.springframework.recipe.project.recipeproject.domain.Recipe;
 import guru.springframework.recipe.project.recipeproject.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +12,11 @@ import java.util.Set;
 @Controller
 public class IndexController {
 
-   @Autowired
-   RecipeService recipeService;
+    RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model) {
---
+++ src/test/java/guru/springframework/recipe/project/recipeproject/controllers/IndexControllerTest.java
@@ -0,0 +1,43 @@
+package guru.springframework.recipe.project.recipeproject.controllers;
+
+import guru.springframework.recipe.project.recipeproject.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        String result = indexController.getIndexPage(model);
+        verify(model,times(1)).addAttribute(eq("recipes"), anySet());
+        verify(recipeService,times(1)).getRecipes();
+
+        assertEquals("index",result);
+
+
+    }
+}
