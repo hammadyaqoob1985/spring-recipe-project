7d2891b033766778bf0a066c30751879cd2af194
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-10-19 21:36:04 +0100

    added in mysql scripts and mysql configuration

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE sfg_dev1;
-CREATE DATABASE sfg_prod1;
+CREATE DATABASE sfg_dev;
+CREATE DATABASE sfg_prod;
 
 #Create database service accounts
 CREATE USER 'sfg_dev_user'@'127.0.0.1' IDENTIFIED BY 'password';
@@ -8,20 +8,20 @@ CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'password';
 CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'password';
 
 #Database grants
-GRANT SELECT ON sfg_dev1.* to 'sfg_dev_user'@'localhost';
-GRANT INSERT ON sfg_dev1.* to 'sfg_dev_user'@'localhost';
-GRANT DELETE ON sfg_dev1.* to 'sfg_dev_user'@'localhost';
-GRANT UPDATE ON sfg_dev1.* to 'sfg_dev_user'@'localhost';
-GRANT SELECT ON sfg_prod1.* to 'sfg_prod_user'@'localhost';
-GRANT INSERT ON sfg_prod1.* to 'sfg_prod_user'@'localhost';
-GRANT DELETE ON sfg_prod1.* to 'sfg_prod_user'@'localhost';
-GRANT UPDATE ON sfg_prod1.* to 'sfg_prod_user'@'localhost';
-GRANT SELECT ON sfg_dev1.* to 'sfg_dev_user'@'%';
-GRANT INSERT ON sfg_dev1.* to 'sfg_dev_user'@'%';
-GRANT DELETE ON sfg_dev1.* to 'sfg_dev_user'@'%';
-GRANT UPDATE ON sfg_dev1.* to 'sfg_dev_user'@'%';
-GRANT SELECT ON sfg_prod1.* to 'sfg_prod_user'@'%';
-GRANT INSERT ON sfg_prod1.* to 'sfg_prod_user'@'%';
-GRANT DELETE ON sfg_prod1.* to 'sfg_prod_user'@'%';
-GRANT UPDATE ON sfg_prod1.* to 'sfg_prod_user'@'%';
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'127.0.0.1';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'127.0.0.1';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'127.0.0.1';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'127.0.0.1';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'127.0.0.1';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'127.0.0.1';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'127.0.0.1';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'127.0.0.1';
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
 
